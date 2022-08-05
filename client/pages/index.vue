ad6e90914279ba9084a993420acc08979d5c3ac9
Tim Zhang <timzhang3@gmail.com>
2022-08-05 12:44:30 +1000

    Remove dark mode support.

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,28 +1,15 @@
 <template>
-	<div class="container" v-bind:class="attachThemeClass">
+	<div class="container">
 		<h1>Transperler</h1>
 		<div class="bg-green-200">Homepage</div>
-		<DarkModeToggler />
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import DarkModeToggler from '~/components/DarkModeToggler.vue';
-import { themeStore } from '~/store';
 
 export default Vue.extend({
 	name: 'IndexPage',
-	components: { DarkModeToggler },
-	computed: {
-		isDarkMode() {
-			return themeStore.isDarkMode;
-		},
-	},
-	methods: {
-		attachThemeClass() {
-			if (this.isDarkMode) return 'dark';
-		},
-	},
+	components: {},
 });
 </script>
