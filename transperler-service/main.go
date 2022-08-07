0bb3f1fdbda47e9f227a89cd972a29ad9668c9c8
Tim Zhang <timzhang3@gmail.com>
2022-08-07 17:40:27 +1000

    Adjust port to 3456 instead of 8080

--- transperler-service/main.go
+++ transperler-service/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 	"transperler-service/shellAnalyser"
 	"transperler-service/transpiler"
 
@@ -27,9 +30,15 @@ func setupRouter() *gin.Engine {
 func main() {
 	router := setupRouter()	
 	
+	port, error := strconv.Atoi(os.Getenv("PORT"))
+	if error != nil {
+		panic("Couldn't retrieve the PORT environment variable.")
+	}
+
 	// This used to be `router.Run("localhost:8080")` but this would make the
 	// container unresponsive to queries. Changing it to `0.0.0.0:8080` solves
 	// this issue.
 	// Source: https://stackoverflow.com/questions/72783444/docker-go-server-does-not-respond
-	router.Run("0.0.0.0:8080")
+	fmt.Printf("Listening at 0.0.0.0:%d", port)
+	router.Run(fmt.Sprintf("0.0.0.0:%d", port))
 }
