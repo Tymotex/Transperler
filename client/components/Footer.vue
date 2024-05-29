1a5d2b00cc60a2e9292b6aba1c90743e788a88ac
Tim Zhang <admin@timz.dev>
2024-05-29 19:47:14 +1000

    Add at-a-glance info and tweak colours for better contrast.

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -1,11 +1,33 @@
 <template>
 	<footer class="max-w-md mx-auto text-center mt-5">
-		<p>
-			Built with <strong>Vue.js</strong> and <strong>Nuxt.js</strong>. See the
-			<a href="https://github.com/Tymotex/Transperler" class="text-blue-200 underline"
-				>repository here</a
-			>.
+		<p class="mb-2"><strong>At a glance...</strong></p>
+		<ul class="quick-info-list text-gray-300">
+			<li>
+				Supports most of the control structures and builtin commands in the POSIX Shell
+				standard.
+			</li>
+			<li>Aims to preserve comments and indentation in the Shell source code.</li>
+			<li>
+				Built with <strong>Vue.js</strong> and <strong>Nuxt.js</strong>. See the
+				<a href="https://github.com/Tymotex/Transperler" class="text-blue-200 underline"
+					>repository here</a
+				>.
+			</li>
+		</ul>
+		<p class="mt-8 text-gray-400">
+			Liked this? See more random software engineering projects at
+			<a href="https://timz.dev" class="underline" target="_blank" rel="noopener noreferrer"
+				>timz.dev</a
+			>! 🤓
 		</p>
 		<p class="font-light text-gray-600 text-sm my-2">&copy; Tim Zhang 2024</p>
 	</footer>
 </template>
+
+<style scoped lang="scss">
+.quick-info-list {
+	text-align: left;
+	list-style-type: circle;
+	padding-left: 24px;
+}
+</style>
--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,14 +1,10 @@
 <template>
-	<header class="max-w-lg p-5 mx-auto my-0">
+	<header class="max-w-lg p-5 mx-auto mt-12">
 		<h1 class="title font-bold text-3xl text-center">Transperler</h1>
-		<p class="description text-center my-3">
-			A transpiler written in Perl 5 that converts POSIX-compatible shell scripts into an
-			equivalent Perl 5 script.
+		<p class="description text-center my-3 text-gray-300">
+			A transpiler that converts POSIX-compatible Shell scripts into an equivalent Perl 5
+			script.
 		</p>
 		<span aria-hidden="true" class="block text-center text-xl mb-3"> 🐚 → ⚪ </span>
 	</header>
 </template>
-<style scoped>
-header {
-}
-</style>
